1493e97b28d1fe3bb91eac33d327b7ca2ae88ab3
Stensis <irenenjuguna98@gmail.com>
2022-08-30 11:06:26 +0300

    commit tree 5d0f1427bb07e360019fb3c1456085195b23240d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    describe ".create_table" do
+        it 'creates the dogs table in the database' do
+          DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+          Dog.create_table
+          table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
+          expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
+        end
+      end
+    end
+    describe ".drop_table" do
+        it 'drops the dogs table from the database' do
+          Dog.drop_table
+          table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
+          expect(DB[:conn].execute(table_check_sql)[0]).to eq(nil)
+        end
+      end
+    end
 end
